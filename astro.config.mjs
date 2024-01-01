1d2cfeac0e99ec2fc15681434cde57dffc239839
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-01-01 17:34:08 +0100

    astro i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,11 @@ import tailwind from "@astrojs/tailwind"
 export default defineConfig({
   site: "https://test-astro-i18n.vercel.app",
   integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en", "fr"],
+    routing: {
+      prefixDefaultLocale: true,
+    }
+  },
 })
